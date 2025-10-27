48045259dcc227e4b9889bffce9eb32a22116bcb
shroommrdoom-glitch <shroommrdoom@gmail.com>
2025-10-26 20:16:46 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+$host = "mysql-sadboyz.alwaysdata.net"; 
+$dbname = "sadboyz_bd_colegio"; 
+$username = "sadboyz"; 
+$password = "SPKDENJI27/_/"; 
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch(PDOException $e) {
+    echo "Error de conexión a AlwaysData: " . $e->getMessage();
+    $conn = null;
+}
+?>
